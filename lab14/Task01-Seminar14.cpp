7d2c46364fca2346958597d2e28833f0de9cee4b
Viktoria035 <115097521+Viktoria035@users.noreply.github.com>
2023-01-16 11:31:05 +0200

    Create Task01-Seminar14.cpp

---
+++ lab14/Task01-Seminar14.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+const int MAX = 5;
+//Да се напише рекурсивна функция, която намира
+//сумата на елементите на масив от цели числа.
+void initialize(int* array, int size) {
+	for (int i = 0; i < size; i++)
+	{
+		std::cin >> array[i];
+	}
+}
+
+int sum(int* array, int index)
+{
+	if (index == MAX)
+	{
+		return 0;
+	}
+	return array[index] + sum(array, index + 1);
+	
+	/*return (index == MAX) ? 0 : sum(array, index + 1)+array[index];*/
+}
+
+int main()
+{
+	int myArray[MAX] = { 0, };
+	int result = 0;
+	initialize(myArray, MAX);
+	std::cout << sum(myArray, 0);
+
+
+	return 0;
+}
