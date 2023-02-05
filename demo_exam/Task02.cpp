5a121da71332c635280fa2d4ddeb212f4a969155
Daniel Jordanov <62522030+danielgo26@users.noreply.github.com>
2023-02-05 11:31:04 +0200

    Add tasks 01-03 from demo exam
    Sending you the three task. Made them for nearly an hour. Having only one issue with the delete command in the first task.

---
+++ demo_exam/Task02.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+const double epsilon = 0.00000000001;
+
+double Abs(double num)
+{
+	return num > 0 ? num : -num;
+}
+
+bool CheckIsSquare(double x1, double y1, double x2, double y2)
+{
+	if (Abs((Abs(x1 - x2) - Abs(y1 - y2))) < epsilon)
+	{
+		return true;
+	}
+	return false;
+}
+
+int main()
+{
+	//task02
+	double x1, x2, y1, y2;
+	std::cin >> x1 >> y1 >> x2 >> y2;
+	if (CheckIsSquare(x1, y1, x2, y2))
+	{
+		std::cout << "square!";
+	}
+	else
+	{
+		std::cout << "not square!";
+	}
+}
