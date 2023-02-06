805a7ec34bd1cdd10bfaa49360fd7aa618ea782f
Viktoria035 <115097521+Viktoria035@users.noreply.github.com>
2023-02-06 10:22:34 +0200

    Update CheckSquare.h

--- demo_exam/CheckSquare.h
+++ demo_exam/CheckSquare.h
@@ -11,9 +11,15 @@ double campareDoubles(double a, double b, double EPSILON = 0.0000001)
 	return (Abs(a - b) < EPSILON) ? true : false;
 }
 
-bool checkSquare(double x1, double y1, double x2, double y2,double (*myAbs) (double))
+bool checkSquare(double x1, double y1, double x2, double y2)
 {
-	if (campareDoubles(myAbs(x1-x2),myAbs(y1-y2)))
+	if ((campareDoubles(x1, x2) && campareDoubles(y1, y2))
+		|| campareDoubles(x1, x2) 
+		|| campareDoubles(y1, y2))
+	{
+		return false;
+	}
+	if (campareDoubles(Abs(x1-x2),Abs(y1-y2)))
 	{
 		return true;
 	}
